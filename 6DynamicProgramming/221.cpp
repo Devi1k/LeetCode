963953c07d595a55be9ac31d3c874a5c9cab1f8f
Devi1k <570541309@qq.com>
2022-03-29 13:37:38 +0800

    Chapter 6 2 dimension dp

--- 6DynamicProgramming/221.cpp
+++ 6DynamicProgramming/221.cpp
@@ -14,8 +14,8 @@ public:
             for (int j = 1; j <= n; ++j) {
                 if (matrix[i - 1][j - 1] == '1')
                     dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                max_side = max(max_side, dp[i][j]);
             }
-            max_side = max(max_side, dp[i][j]);
         }
         return max_side * max_side;
     }
