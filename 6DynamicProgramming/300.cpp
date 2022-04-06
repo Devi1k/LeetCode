633e37df843a7453eae4fe6e32ff04eb7dc44320
Devi1k <570541309@qq.com>
2022-04-06 16:58:43 +0800

    Chapter 6 sub sequence

---
+++ 6DynamicProgramming/300.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 倪泽溥 on 2022/4/1.
+//
+#include "../head.h"
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int> &nums) {
+        int n = nums.size();
+        if (n <= 1) return n;
+        vector<int> dp(n);
+        dp.push_back(nums[0]);
+        for (int i = 1; i < n; ++i) {
+            if (dp.back() < nums[i])
+                dp.push_back(nums[i]);
+            else {
+                auto itr = lower_bound(dp.begin(), dp.end(), nums[i]);
+                *itr = nums[i];
+            }
+        }
+        return dp.size();
+    }
+};
