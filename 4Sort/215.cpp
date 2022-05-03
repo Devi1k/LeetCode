b9dd02ee08348c692e08f835f897bc5ea6ca4ded
Devi1k <570541309@qq.com>
2022-05-03 17:51:50 +0800

    高频

--- 4Sort/215.cpp
+++ 4Sort/215.cpp
@@ -19,12 +19,15 @@ public:
     }
 
     inline int randomPartition(vector<int> &a, int l, int r) {
+        // 随机一个索引，+l保证其在l与r之间
         int i = rand() % (r - l + 1) + l;
+        // 把随机选择的数作为标志对比
         swap(a[i], a[r]);
         return partition(a, l, r);
     }
 
     inline int partition(vector<int> &a, int l, int r) {
+        // 快速排序，如果小于标志
         int x = a[r], i = l - 1;
         for (int j = l; j < r; ++j) {
             if (a[j] <= x) {
@@ -36,14 +39,14 @@ public:
     }
 
     int findKthLargest(vector<int> &nums, int k) {
-        srand(time(0));
+//        srand(time(0));
         return quickSelect(nums, 0, nums.size() - 1, nums.size() - k);
     }
 };
 
 
 int main() {
-    vector<int> nums = {3, 2, 1, 5, 6, 4};
+    vector<int> nums = {3, 2, 5, 1, 6, 4};
     int k = 2;
     Solution solution;
     cout << solution.findKthLargest(nums, k);
---
+++ HighFrequency/25.cpp
@@ -0,0 +1,40 @@
+//
+// Created by 倪泽溥 on 2022/5/2.
+//
+#include "../head.h"
+
+class Solution {
+public:
+    pair<ListNode *, ListNode *> reverse(ListNode *head, ListNode *tail) {
+        ListNode *prev = tail->next;
+        ListNode *p = head;
+        while (prev != tail) {
+            ListNode *next = p->next;
+            p->next = prev;
+            prev = p;
+            p = next;
+        }
+        return {tail, head};
+    }
+
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        ListNode *dummy = new ListNode();
+        dummy->next = head;
+        ListNode *prev = dummy;
+        while (head) {
+            ListNode *tail = prev;
+            for (int i = 0; i < k; ++i) {
+                tail = tail->next;
+                if (!tail)
+                    return dummy->next;
+            }
+            ListNode *next = tail->next;
+            tie(head, tail) = reverse(head, tail);
+            prev->next = head;
+            tail->next = next;
+            prev = tail;
+            head = tail->next;
+        }
+        return dummy->next;
+    }
+};
