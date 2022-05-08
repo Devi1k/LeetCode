c0dfb684b928290ca322c20f4930a95ec7d22be1
Devi1k <570541309@qq.com>
2022-05-08 12:11:15 +0800

    对比

--- 6DynamicProgramming/10.cpp
+++ 6DynamicProgramming/10.cpp
@@ -1,38 +1,47 @@
 //
 // Created by 倪泽溥 on 2022/4/8.
 //
+#include <string>
+#include <iostream>
+#include <vector>
 
-#include "../head.h"
+using namespace std;
 
 class Solution {
 public:
-    bool isMatch(string s, string p) {
+    bool match(string s, string p) {
         int m = s.size(), n = p.size();
         vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
         dp[0][0] = true;
-        for (int i = 1; i < n + 1; ++i) {
-            if (p[i - 1] == '*')
+        for (int i = 2; i <= n; ++i) {
+            if (p[i - 1] == '*') {
                 dp[0][i] = dp[0][i - 2];
+            }
         }
         for (int i = 1; i < m + 1; ++i) {
             for (int j = 1; j < n + 1; ++j) {
-                if (p[j - 1] == '.')
-                    dp[i][j] = dp[i - 1][j];
-                else if (p[j - 1] != '*')
-                    dp[i][j] = dp[i - 1][j - 1] && p[j - 1] == s[i - 1];
-                else if (p[j - 2] != s[i - 1] && p[j - 2] != '.')
-                    dp[i][j] = dp[i][j - 2];
-                else {
-                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j] || dp[i][j - 2];
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 1] == '.'));
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
                 }
             }
         }
         return dp[m][n];
+
     }
 };
 
+
 int main() {
+    string s, p;
+    cin >> s;
+    cin >> p;
     Solution solution;
-    cout << solution.isMatch("aab", "c*a*b");
+    if (solution.match(s, p)) {
+        cout << "true";
+    } else {
+        cout << "false";
+    }
 
 }
