eab66d5e8bbe3d8858039854d0eb310e9ee9eaa7
zepu.ni <Nzp19990421.123>
2022-09-27 13:59:09 +0800

    practice0927

--- 13Tree/144.cpp
+++ 13Tree/144.cpp
@@ -1,25 +1,54 @@
 //
 // Created by 倪泽溥 on 2022/4/25.
 //
-#include "../head.h"
+#include "iostream"
+#include "vector"
+#include "stack"
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
+};
 
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode *root) {
-        vector<int> ans;
-        if (!root)
-            return ans;
-        stack<TreeNode *> s;
-        s.push(root);
-        while (!s.empty()) {
-            TreeNode *node = s.top();
-            s.pop();
-            ans.emplace_back(node->val);
-            if (node->right)
-                s.push(node->right);
-            if (node->left)
-                s.push(node->left);
-        }
-        return ans;
+//    vector<int> preorderTraversal(TreeNode *root) {
+//        vector<int> res;
+//        if (!root) return res;
+//        traversal(root, res);
+//        return res;
+//    }
+
+    void traversal(TreeNode *root, vector<int> &res) {
+        if (!root) return;
+        res.push_back(root->val);
+        if (root->left)
+            traversal(root->left, res);
+        if (root->right)
+            traversal(root->right, res);
     }
+
+    vector<int> preorderTraversal(TreeNode *root) {
+        stack<TreeNode *> tree;
+        vector<int> res;
+        if (!root) return res;
+        tree.push(root);
+        while (!tree.empty()) {
+            TreeNode *node = tree.top();
+            tree.pop();
+            res.push_back(node->val);
+            if (node->right)
+                tree.push(node->right);
+            if (node->left)
+                tree.push(node->left);
+        }
+        return res;
+    }
+
+
 };
