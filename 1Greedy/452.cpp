055b7edb03016e2c48006a834c69676a483a3466
Devi1k <570541309@qq.com>
2022-11-21 19:26:29 +0800

    leetcode 11.21

--- 1Greedy/452.cpp
+++ 1Greedy/452.cpp
@@ -11,19 +11,19 @@ using namespace std;
 
 class Solution {
 public:
+    static bool cmp(vector<int> &a, vector<int> &b) {
+        return a[0] < b[0];
+    }
+
     int findMinArrowShots(vector<vector<int>> &points) {
-        if (points.empty())
-            return 0;
-        sort(points.begin(), points.end(), []
-                (vector<int> a, vector<int> b) {
-            return a[1] < b[1];
-        });
-        int res = 1, pos = points[0][1];
-        for (const vector<int> &balloon: points) {
-            if (balloon[0] > pos) {
-                res++;
-                pos = balloon[1];
-            }
+        if (points.size() < 1) return 0;
+        int res = 1;
+        sort(points.begin(), points.end(), cmp);
+        for (int i = 1; i < points.size(); ++i) {
+            if (points[i][0] > points[i - 1][1])
+                ++res;
+            else
+                points[i][1] = min(points[i - 1][1], points[i][1]);
         }
         return res;
     }
